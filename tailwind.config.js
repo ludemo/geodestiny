a85397d8cd2884f9ab8f4a6735f35042c90a32d2
ludemo <azevallosa@unsa.edu.pe>
2024-10-05 14:38:10 -0500

    maquetado base de opciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pixelify: ['"Pixelify Sans"', 'sans-serif'],
+        tiny: ['"Tiny5"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
